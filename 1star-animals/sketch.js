094e966adbdbdfcf15a0f1be13447f3e6e837fa1
Anya795 <psd540@york.ac.uk>
2026-02-19 13:46:56 +0000

    hiii

--- 1star-animals/sketch.js
+++ 1star-animals/sketch.js
@@ -1,13 +1,21 @@
 let animal1;
+let bear;
+let cat;
+let monkey;
 
 function setup() {
+    createCanvas(400,400);
     // TODO create the canvas
     animal1 = new Animal(); // TODO fill in the parameters
 }
 
 function draw() {
+    background(0);
     // TODO draw the background
     //animal1.display();
+    image(bear, 0,0);
+    image(cat, 20, 100);
+    image(monkey, 40, 100);
 }
 
 function keyPressed() {
@@ -15,4 +23,31 @@ function keyPressed() {
     if (key === "w") {
         animal1.moveY(-10);
     } // etc
+}
+
+class Animal{
+    x;
+    y;
+    speedX;
+    speedY;
+}
+
+constuctor();
+this.x = 50;
+this.y = 50;
+this.loadImage = bear;
+this.loadImage = cat;
+this.loadImage = monkey;
+    //this.speedX = 3;
+    //this.speedy = 3;
+
+    function preload() {
+        bear = loadImage ('/assets/bear.jpg');
+        cat = loadImage ('/assets/cat.jpg');
+        monkey = loadImage ('/assets/monkey.jpg');
+
+    }
+
+function display(){
+
 }
